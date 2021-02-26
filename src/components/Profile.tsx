0737c9b93268a21b2c21415fb52f0ac6a6264463
Leo Costa <leonardoagostininho@gmail.com>
2021-02-26 09:32:20 -0300

    2 last days, NLW4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles  from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
     return (
         <div className={styles.profileContainer}>
             <img src="https://i.ytimg.com/vi/JyFM-p4Kbh4/maxresdefault.jpg" alt="Leonardo A."/>
@@ -8,7 +11,7 @@ export function Profile() {
                 <strong>Leonardo Agostini Costa</strong>
                 <p>
                     <img src="icons/level.svg" alt=""/>
-                    Level 1
+                    {level}
                 </p>
 
             </div>
